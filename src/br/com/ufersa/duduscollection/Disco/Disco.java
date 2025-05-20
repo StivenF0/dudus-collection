23897d6547e7534e8fa80c607204110909962c49
StivenF0 <stivenfonseca201@gmail.com>
2025-05-20 17:38:04 -0300

    feat: implementação básica da classe Disco

--- src/br/com/ufersa/duduscollection/Disco/Disco.java
+++ src/br/com/ufersa/duduscollection/Disco/Disco.java
@@ -1,5 +1,62 @@
 package br.com.ufersa.duduscollection.Disco;
 
 public class Disco {
-    // TODO: Implementar classe
+    private String titulo, estilo;
+    private int anoLancamento;
+    private int qtdExemplares;
+    private double valorAluguel;
+
+    public Disco() {}
+
+    public Disco(String titulo, String estilo, int anoLancamento, int qtdExemplares, double valorAluguel) {
+        setTitulo(titulo);
+        setEstilo(estilo);
+        setQtdExemplares(qtdExemplares);
+        setValorAluguel(valorAluguel);
+        setAnoLancamento(anoLancamento);
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public void setQtdExemplares(int qtdExemplares) {
+        if (qtdExemplares >= 0) {
+            this.qtdExemplares = qtdExemplares;
+        }
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        if (valorAluguel >= 0) {
+            this.valorAluguel = valorAluguel;
+        }
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getEstilo() {
+        return this.estilo;
+    }
+
+    public int getQtdExemplares() {
+        return this.qtdExemplares;
+    }
+
+    public double getValorAluguel() {
+        return this.valorAluguel;
+    }
+
+    public int getAnoLancamento() {
+        return this.anoLancamento;
+    }
 }
