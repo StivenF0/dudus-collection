944e313a8d8748ca54edb3921c9565a0de7052ab
StivenF0 <stivenfonseca201@gmail.com>
2025-05-20 17:47:57 -0300

    feat: implementação básica da classe Livro

--- src/br/com/ufersa/duduscollection/Livro/Livro.java
+++ src/br/com/ufersa/duduscollection/Livro/Livro.java
@@ -1,5 +1,66 @@
 package br.com.ufersa.duduscollection.Livro;
 
 public class Livro {
-    // TODO: Implementar classe
+    private String nome, genero;
+    private int anoLancamento, qtdPaginas, qtdExemplares;
+    private double valorAluguel;
+
+    public Livro() {}
+
+    public Livro(String nome, String genero, int anoLancamento, int qtdPaginas, int qtdExemplares, double valorAluguel) {
+        setNome(nome);
+        setGenero(genero);
+        setAnoLancamento(anoLancamento);
+        setQtdPaginas(qtdPaginas);
+        setQtdExemplares(qtdExemplares);
+        setValorAluguel(valorAluguel);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setQtdPaginas(int qtdPaginas) {
+        this.qtdPaginas = qtdPaginas;
+    }
+
+    public void setQtdExemplares(int qtdExemplares) {
+        this.qtdExemplares = qtdExemplares;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public int getQtdPaginas() {
+        return qtdPaginas;
+    }
+
+    public int getQtdExemplares() {
+        return qtdExemplares;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
 }
