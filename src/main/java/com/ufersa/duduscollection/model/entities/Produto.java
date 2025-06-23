6097c7a95cc99255b271f5cbfc02aa4da27ad9c8
StivenF0 <stivenfonseca201@gmail.com>
2025-06-23 18:49:56 -0300

    Add Produto model

---
+++ src/main/java/com/ufersa/duduscollection/model/entities/Produto.java
@@ -0,0 +1,75 @@
+package com.ufersa.duduscollection.model.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="Produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String nome;
+
+    @Column(name="data_lancamento")
+    private Date dataLancamento;
+
+    @Column(name="qtd_exemplares", nullable=false, columnDefinition="int default 0")
+    private int qtdExemplares;
+
+    @Column(name="valor_aluguel", nullable=false, precision=10, scale=2)
+    private BigDecimal valorAluguel;
+
+    public Produto() {}
+
+    public Produto(String nome, Date dataLancamento, int qtdExemplares, BigDecimal valorAluguel) {
+        setNome(nome);
+        setDataLancamento(dataLancamento);
+        setQtdExemplares(qtdExemplares);
+        setValorAluguel(valorAluguel);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public BigDecimal getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public int getQtdExemplares() {
+        return qtdExemplares;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDataLancamento(Date dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public void setQtdExemplares(int qtdExemplares) {
+        this.qtdExemplares = qtdExemplares;
+    }
+
+    public void setValorAluguel(BigDecimal valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+}
