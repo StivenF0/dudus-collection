912f2c7f7916066e1f5d3162ea9022b2a3af1fb1
StivenF0 <stivenfonseca201@gmail.com>
2025-07-30 18:17:32 -0300

    refactor: cleanup code on ProdutoService.java

--- src/main/java/com/ufersa/duduscollection/model/services/ProdutoService.java
+++ src/main/java/com/ufersa/duduscollection/model/services/ProdutoService.java
@@ -4,7 +4,6 @@ import com.ufersa.duduscollection.model.dao.ProdutoDAO;
 import com.ufersa.duduscollection.model.entities.Produto;
 
 import java.util.List;
-import java.util.Optional;
 
 public class ProdutoService {
     private final ProdutoDAO produtoDAO;
